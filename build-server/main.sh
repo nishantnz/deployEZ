fce99a952e61eb2a912b39abd86e0c24157311e9
nishantnz <nishant.z@somaiya.edu>
2024-06-03 23:24:49 +0530

    minor changes

--- build-server/main.sh
+++ build-server/main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export GIT_REPOSITORY_URL = "$GIT_REPOSITORY_URL"
-
+export GIT_REPOSITORY_URL="$GIT_REPOSITORY_URL"
 
 echo "Cloning the Git URL"
 git clone "$GIT_REPOSITORY_URL" /home/app/output
